56a14916f0b259cc154a6d24f353ddbd80ad0421
cquiros <christian.quiros@advantechcr.com>
2020-03-28 23:01:18 -0600

    change title

--- api/Startup.cs
+++ api/Startup.cs
@@ -24,7 +24,7 @@ namespace api
                 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Generate Random Data API", Version = "v1" });
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "NEW Generate Random Data API", Version = "v1" });
             });
         }
 
